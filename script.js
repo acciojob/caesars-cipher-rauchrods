e60cc5cefb9ea0fa06ddb11499520d68e7ffab51
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-17 13:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 // Your Script here.
 function rot13(encryptedstring){
+
 	var lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
+  "A": "N",
+  "B": "O",
+  "C": "P",
+  "D": "Q",
+  "E": "R",
+  "F": "S",
+  "G": "T",
+  "H": "U",
+  "I": "V",
+  "J": "W",
+  "K": "X",
+  "L": "Y",
+  "M": "Z",
+  "N": "A",
+  "O": "B",
+  "P": "C",
+  "Q": "D",
+  "R": "E",
+  "S": "F",
+  "T": "G",
+  "U": "H",
+  "V": "I",
+  "W": "J",
+  "X": "K",
+  "Y": "L",
+  "Z": "M",
   "?": "?",
   ",": ",",
 };
 
 
-var encystr= "THIS IS A SAMPLE TEXT";
+
 var decrystr= "";
-for(let i=0;i<encystr.length;i++){
-  if(encystr.charCodeAt(i)==32){
+for(let i=0;i<encryptedstring.length;i++){
+  if(encryptedstring.charCodeAt(i)==32){
     decrystr+=" ";
   }
    else{
-    decrystr+=lookup[encystr.charAt(i)];
+    decrystr+=lookup[encryptedstring.charAt(i)];
    }
 }
 
-console.log(decrystr);
+ return decrystr;
+	
 }
