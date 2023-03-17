d5eaef47c468bd7671638de6d51e723ce128ecbf
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-17 13:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 // Your Script here.
+function rot13(encryptedstring){
+	var lookup = {
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
+};
+
+
+var encystr= "THIS IS A SAMPLE TEXT";
+var decrystr= "";
+for(let i=0;i<encystr.length;i++){
+  if(encystr.charCodeAt(i)==32){
+    decrystr+=" ";
+  }
+   else{
+    decrystr+=lookup[encystr.charAt(i)];
+   }
+}
+
+console.log(decrystr);
+}
